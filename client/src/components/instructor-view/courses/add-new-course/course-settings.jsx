a2817d70e22020714062d84a737491dcfc714164
Motar-Dev <mohammed.motar10@gmail.com>
2026-07-18 21:58:54 +0200

    create-new-course add title and input file to course settings file

--- client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
+++ client/src/components/instructor-view/courses/add-new-course/course-settings.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Label } from '../../../ui/label';
+import { Input } from '../../../ui/input';
 
 function CourseSettings() {
-  return <div>CourseSettings</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Course Settings</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className='flex flex-col gap-3'>
+          <Label>Upload Course Image</Label>
+          <Input type='file' accept='image/*' />
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default CourseSettings;
